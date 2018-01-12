5f065b21bf4d74fd5c67cc5ad4147eef16c0472b
arnold5sweet <arnold.kmulera@gmail.com>
2018-01-12 15:42:58 +0300

    Jquery Cool Effects

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,6 +1,26 @@
+// noinspection JSValidateTypes
 /**
  * Creating some JQuery for cool effects on the Web page.
  */
 $(document).ready(function () {
 
+    //Adding the waypoints plugin and 'direction' is to detect whether the URL is scrolling up the
+    // webpage or down
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky')
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    //    Happens before we hit the js--section-features
+    }, {
+        offset: '60px'
+    });
+
+
+    // var waypoints = $('#handler-first').waypoint(function (direction) {
+    //     notify(this.element.id + ' hit 25% from top of window')
+    // }, {
+    //     offset: '25%'
+    // })
 });
